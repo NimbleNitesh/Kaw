94edf9844eec33482aac973759a5a4f1c3e1f231
NimbleNitesh <nitesh620137@gmail.com>
2024-06-13 21:10:54 +0530

    chore: finished GraphQL setup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,39 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constant";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import "reflect-metadata";
 
 const main = async () => {
-    // console.log(mikroOrmConfig);
     const orm = await MikroORM.init(mikroOrmConfig);
     await orm.getMigrator().up();
     const em = orm.em.fork()
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false
+        }),
+        context: () => ({em: em})
+    });
+
+    await apolloServer.start();    
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log("Server started at port 4000");
+    });
     // const post = em.create(Post, {title: 'First Post test'} as Post);
     // await em.persist(post).flush();
 
-    const post = await em.find(Post, {});
-    console.log(post);
+    // const post = await em.find(Post, {});
+    // console.log(post);
 
 };
 console.log('Nitesh Srivastava')
